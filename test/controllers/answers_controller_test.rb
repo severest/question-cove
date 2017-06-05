e2e7e2a1430148f7f5bb218020f3cfbbf141cb6f
Sean Everest <sean.b.everest@gmail.com>
2017-06-05 09:06:18 -0700

    answers testing

--- test/controllers/answers_controller_test.rb
+++ test/controllers/answers_controller_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class AnswersControllerTest < ActionController::TestCase
+  setup do
+    @answer = answers(:one)
+  end
+
   test "should update answer" do
     session[:user_id] = users(:sean).id
     patch :update, params: { id: answers(:one), answer: { text: 'updated text!' } }
@@ -10,7 +14,7 @@ class AnswersControllerTest < ActionController::TestCase
   test "should destroy answer" do
     session[:user_id] = users(:sean).id
     assert_difference('Answer.count', -1) do
-      delete :destroy, params: { id: answers(:one) }
+      delete :destroy, params: { id: @answer }
     end
     assert_redirected_to questions(:one)
   end
@@ -27,8 +31,36 @@ class AnswersControllerTest < ActionController::TestCase
   test "should not destroy answer of another user" do
     session[:user_id] = users(:john).id
     assert_no_difference('Answer.count') do
-      delete :destroy, params: { id: answers(:one) }
+      delete :destroy, params: { id: @answer }
     end
     assert_redirected_to questions(:one)
+  end
+
+  test "should get edit" do
+    session[:user_id] = users(:sean).id
+    get :edit, params: { id: @answer }
+    assert_response :success
+  end
+
+  test "should not get edit for not your answer" do
+    session[:user_id] = users(:john).id
+    get :edit, params: { id: @answer }
+    assert_redirected_to questions(:one)
+  end
+
+  test "should update question" do
+    session[:user_id] = users(:sean).id
+    patch :update, params: { id: @answer, answer: { text: 'changed my answer' } }
+    @answer.reload
+    assert_equal @answer.text, 'changed my answer'
+    assert_redirected_to @answer.question
+  end
+
+  test "should not update question" do
+    session[:user_id] = users(:john).id
+    patch :update, params: { id: @answer, answer: { text: 'changed my answer' } }
+    @answer.reload
+    assert_not_equal @answer.text, 'changed my answer'
+    assert_redirected_to @answer.question
   end
 end
