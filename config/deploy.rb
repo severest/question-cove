43e17d0c693976853c9854ef57ce031c7eb0042b
Sean Everest <me@seaneverest.com>
2017-09-11 16:47:50 -0700

    update gems

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.9.0'
+lock '3.9.1'
 
 set :application, 'question-cove'
 set :repo_url, 'https://github.com/severest/question-cove.git'
