d4df8956ee4fe3d7dfced0b8ea3d19a9dd077bc9
Sean Everest <sean.b.everest@gmail.com>
2018-09-05 23:33:05 -0700

    user factory

--- test/factories/user.rb
+++ test/factories/user.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     sequence(:email) { |n| "person#{n}@example.com" }
     provider { 'test' }
     sequence(:uid) { |n| "test#{n}" }
+    sequence(:name) { |n| "person#{n}" }
   end
 end
