85b3502fe3e36b73bfd5676bd42fe195fcd3ad08
Sean Everest <me@seaneverest.com>
2019-12-25 00:34:58 -0800

    email domain whitelist as env variable

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,4 +2,4 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_SECRET'], scope: 'profile,email', image_aspect_ratio: 'square', image_size: 48, access_type: 'online', name: 'google', verify_iss: false
 end
 
-Rails.application.config.domain_whitelist = Regexp.union(/.*/)
+Rails.application.config.domain_whitelist = Regexp.new(ENV['DOMAINS'] || '.*')
